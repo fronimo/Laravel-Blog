cefeec99d72da03a8b2d185f75bed2c45dc0029e
fronimo <alfredo.morote@gmail.com>
2021-10-11 15:37:20 -0500

    creation of random records in Curso and User table  using factory method

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Curso;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $this->call(CursoSeeder::class);
+        // $this->call(CursoSeeder::class);
+        User::factory(10)->create();
+        Curso::factory(50)->create();
     }
 }
